8fea6e5cd55c85f5e08fd70d6e2ab7180a85df21
PeterNguyen-BK <thuan156109@gmail.com>
2021-06-03 23:28:15 +0700

    update code

--- src/views/home.php
+++ src/views/home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang chủ</title>
-    <link rel="stylesheet" href="/assignment2/public/styles/home.css">
+    <link rel="stylesheet" href="public/styles/home.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
@@ -14,7 +14,7 @@
     <?php require_once "./src/views/layouts/header.php"?>
     <div class="content">
         <?php require_once "./src/views/layouts/login.php"?>
-        <img class="animate__animated animate__bounceInRight icon-messenger" src="/assignment2/public/img/messenger.svg" alt="messenger">
+        <img class="animate__animated animate__bounceInRight icon-messenger" src="public/img/messenger.svg" alt="messenger">
         <div class="message">
           <div class="slide-effect animate__animated animate__slideInRight ">
             <p>Chat with me for more information!</p>
@@ -31,21 +31,21 @@
             </ol>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img class="d-block w-100 animate__animated animate__fadeIn" src="/assignment2/public/img/bg1.jpg" alt="First slide">
+                <img class="d-block w-100 animate__animated animate__fadeIn" src="public/img/bg1.jpg" alt="First slide">
                 <div class="carousel-caption d-none d-md-block">
                   <h5 class="animate__animated animate__zoomInDown slogan">Thân thiện với khách hàng - Chăm chỉ - Cẩn thận</h5>
                   <p class="animate__animated animate__zoomInDown slogan">Dọn dẹp căn nhà của bạn</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img class="d-block w-100 animate__animated animate__fadeIn" src="/assignment2/public/img/bg2.jpg" alt="Second slide">
+                <img class="d-block w-100 animate__animated animate__fadeIn" src="public/img/bg2.jpg" alt="Second slide">
                 <div class="carousel-caption d-none d-md-block">
                   <h5 class="animate__animated animate__zoomInDown slogan">Thân thiện với khách hàng - Chăm chỉ - Cẩn thận</h5>
                   <p class="animate__animated animate__zoomInDown slogan">Dọn dẹp căn nhà của bạn</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img class="d-block w-100 animate__animated animate__fadeIn" src="/assignment2/public/img/bg3.jpg" alt="Third slide">
+                <img class="d-block w-100 animate__animated animate__fadeIn" src="public/img/bg3.jpg" alt="Third slide">
                 <div class="carousel-caption d-none d-md-block">
                   <h5 class="animate__animated animate__zoomInDown slogan">Thân thiện với khách hàng - Chăm chỉ - Cẩn thận</h5>
                   <p class="animate__animated animate__zoomInDown slogan">Dọn dẹp căn nhà của bạn</p>
--- src/views/layouts/header.php
+++ src/views/layouts/header.php
@@ -11,8 +11,8 @@
         <a class="navbar-brand1 non-display" href="#">
             <?php
             if ($_GET["url"] == 'Home/' || $_GET["url"] == null) 
-                echo "<img class=\"logo\" src=\"/assignment2/public/img/logo-light-01.png\" alt=\"\">";
-            else echo "<img class=\"logo\" src=\"/assignment2/public/img/logo-dark-01.png\" alt=\"\">";
+                echo "<img class=\"logo\" src=\"public/img/logo-light-01.png\" alt=\"\">";
+            else echo "<img class=\"logo\" src=\"public/img/logo-dark-01.png\" alt=\"\">";
             ?>
             <!-- <img class="logo" src="/assignment2/public/img/logo-dark-01.png" alt=""> -->
         </a>
@@ -32,28 +32,28 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent" style="width: 100%">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                <a class="nav-link" href="/assignment2/Home/">TRANG CHỦ<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="Home/">TRANG CHỦ<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="/assignment2/Introduction/">GIỚI THIỆU</a>
+                <a class="nav-link" href="Introduction/">GIỚI THIỆU</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/assignment2/Information/">DỊCH VỤ</a>
+                    <a class="nav-link" href="Information/">DỊCH VỤ</a>
                 </li>
                 <li>
                     <a class="navbar-brand" href="#">
                         <?php
                         if (!isset($_GET["url"]) || $_GET["url"] == 'Home/' ) 
-                            echo "<img class=\"logo\" src=\"/assignment2/public/img/logo-light-01.png\" alt=\"\">";
-                        else echo "<img class=\"logo\" src=\"/assignment2/public/img/logo-dark-01.png\" alt=\"\">";
+                            echo "<img class=\"logo\" src=\"public/img/logo-light-01.png\" alt=\"\">";
+                        else echo "<img class=\"logo\" src=\"public/img/logo-dark-01.png\" alt=\"\">";
                         ?>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/assignment2/Price">BẢNG GIÁ</a>
+                    <a class="nav-link" href="Price/">BẢNG GIÁ</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/assignment2/Contact/">LIÊN HỆ</a>
+                    <a class="nav-link" href="Contact/">LIÊN HỆ</a>
                 </li>
                 <li>
                     <div class="dropdown search">
