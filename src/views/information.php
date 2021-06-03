8b9c1f96dd365ae04c9e9c46414c7d01cebeb409
PeterNguyen-BK <thuan156109@gmail.com>
2021-06-04 00:03:33 +0700

    update

--- src/views/information.php
+++ src/views/information.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="public/styles/information.css">
+    <link rel="stylesheet" href="../public/styles/information.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
@@ -29,7 +29,7 @@
                         <p class="dichvu"><?php echo $row["name"] ?></p>
                         <div class="line-bottom-small"></div>
                         <p class="content-dichvu"><?php echo substr($row["description"], 0, strrpos($row["description"], " ", -500)) ?>...</p>
-                        <a href="./information/service/<?php echo $row["SID"]; ?>"><button class="btn btn-outline-info" style="margin-bottom: 20px;">Chi tiết</button></a>
+                        <a href="./service/<?php echo $row["SID"]; ?>"><button class="btn btn-outline-info" style="margin-bottom: 20px;">Chi tiết</button></a>
                     </div>
             <?php }?>
                 <!-- <div class="col content-img">
@@ -216,6 +216,6 @@
     <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="public/scripts/script.js"></script>
+    <script src="../public/scripts/script.js"></script>
 </body>
 </html>
--- src/views/introduction.php
+++ src/views/introduction.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giới thiệu</title>
-    <link rel="stylesheet" href="public/styles/introduction.css">
+    <link rel="stylesheet" href="../public/styles/introduction.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
@@ -25,17 +25,17 @@
             <div class="row align-equal">
                 <div class="col-12 col-sm-6 col-md-5 col-lg-6" id="gallery">
                     <div class="col-lg-12 col-md-12 grid-col-1 pd0">
-                        <a href="#"><img src="public/image/h1.jpg" alt="h1" id="heg100"></a>
+                        <a href="#"><img src="../public/image/h1.jpg" alt="h1" id="heg100"></a>
                     </div>
                     <div class="row">
                         <div class="col-lg-4 col-md-12 grid-col-1-3 pd10">
-                            <a href="#"><img src="public/image/h2sm.jpg " alt="h2" class="heg95"></a>
+                            <a href="#"><img src="../public/image/h2sm.jpg " alt="h2" class="heg95"></a>
                         </div>
                         <div class="col-lg-4 col-md-12 grid-col-1-3 pd10">
-                            <a href="#"><img src="public/image/h3sm.jpg" alt="h3" class="heg95"></a>
+                            <a href="#"><img src="../public/image/h3sm.jpg" alt="h3" class="heg95"></a>
                         </div>
                         <div class="col-lg-4 col-md-12 grid-col-1-3 pd10">
-                            <a href="#"><img src="public/image/h4sm.jpg" alt="h4" class="heg95"></a>
+                            <a href="#"><img src="../public/image/h4sm.jpg" alt="h4" class="heg95"></a>
                         </div>
                     </div>
                 </div>
@@ -64,21 +64,21 @@
                 <div class="row">
                     <div class="col-sm-4 pdxq">
                         <div class="p24">
-                            <img src="public/image/icon1.png" alt="icon1" class="pdxq1">
+                            <img src="../public/image/icon1.png" alt="icon1" class="pdxq1">
                             <h5 class="pdxq1h">Thanh toán dễ dàng</h5>
                             <p class="pdxq1p">Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
                     </div>
                     <div class="col-sm-4 pdxq">
                         <div class="p24">
-                            <img src="public/image/icon2.png" alt="icon2" class="pdxq1">
+                            <img src="../public/image/icon2.png" alt="icon2" class="pdxq1">
                             <h5 class="pdxq1h">Thanh toán dễ dàng</h5>
                             <p class="pdxq1p">Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
                     </div>
                     <div class="col-sm-4 pdxq">
                         <div class="p24">
-                            <img src="public/image/icon3.png" alt="icon3" class="pdxq1">
+                            <img src="../public/image/icon3.png" alt="icon3" class="pdxq1">
                             <h5 class="pdxq1h">Thanh toán dễ dàng</h5>
                             <p class="pdxq1p">Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
@@ -96,36 +96,36 @@
                     </div>
                     <div class="row k11">
                         <div class="col-sm-6 col-12 k111">
-                            <img src="public/image/icon4.png" alt="icon4">
+                            <img src="../public/image/icon4.png" alt="icon4">
                             <h5>CÔNG NGHỆ LÀM SẠCH</h5>
                             <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
                         <div class="col-sm-6 col-12 k111">
-                            <img src="public/image/icon5.png" alt="icon5">
-                            <h5>CÔNG NGHỆ LÀM SẠCH</h5>
-                            <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
-                        </div>
-                    </div>
-                    <div class="row k11">
-                        <div class="col-sm-6 col-12 k111">
-                            <img src="public/image/icon6.png" alt="icon6">
-                            <h5>CÔNG NGHỆ LÀM SẠCH</h5>
-                            <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
-                        </div>
-                        <div class="col-sm-6 col-12 k111">
-                            <img src="public/image/icon7.png" alt="icon7">
+                            <img src="../public/image/icon5.png" alt="icon5">
                             <h5>CÔNG NGHỆ LÀM SẠCH</h5>
                             <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
                     </div>
                     <div class="row k11">
                         <div class="col-sm-6 col-12 k111">
-                            <img src="public/image/icon8.png" alt="icon8">
+                            <img src="../public/image/icon6.png" alt="icon6">
                             <h5>CÔNG NGHỆ LÀM SẠCH</h5>
                             <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
                         <div class="col-sm-6 col-12 k111">
-                            <img src="public/image/icon9.png" alt="icon9">
+                            <img src="../public/image/icon7.png" alt="icon7">
+                            <h5>CÔNG NGHỆ LÀM SẠCH</h5>
+                            <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
+                        </div>
+                    </div>
+                    <div class="row k11">
+                        <div class="col-sm-6 col-12 k111">
+                            <img src="../public/image/icon8.png" alt="icon8">
+                            <h5>CÔNG NGHỆ LÀM SẠCH</h5>
+                            <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
+                        </div>
+                        <div class="col-sm-6 col-12 k111">
+                            <img src="../public/image/icon9.png" alt="icon9">
                             <h5>CÔNG NGHỆ LÀM SẠCH</h5>
                             <p>Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn.</p>
                         </div>
@@ -162,18 +162,18 @@
                         </div>
                         <div class="row">
                             <div class="col-6 cao" id="logo1">
-                                <img src="public/image/logo_01-1.png" alt="logo1" class="lg1">
+                                <img src="../public/image/logo_01-1.png" alt="logo1" class="lg1">
                             </div>
                             <div class="col-6 cao" id="logo2">
-                                <img src="public/image/logo_02-1.png" alt="logo1" class="lg1">
+                                <img src="../public/image/logo_02-1.png" alt="logo1" class="lg1">
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-6 cao" id="logo3">
-                                <img src="public/image/logo_03-1.png" alt="logo1" class="lg1">
+                                <img src="../public/image/logo_03-1.png" alt="logo1" class="lg1">
                             </div>
                             <div class="col-6 cao" id="logo4">
-                                <img src="public/image/logo_04-1.png" alt="logo1" class="lg1">
+                                <img src="../public/image/logo_04-1.png" alt="logo1" class="lg1">
                             </div>
                         </div>
                     </div>
@@ -196,6 +196,6 @@
     <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="public/scripts/script.js"></script>
+    <script src="../public/scripts/script.js"></script>
 </body>
 </html>
