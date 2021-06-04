9ae4652156168b7750331c99225d180270e3477a
PeterNguyen-BK <thuan156109@gmail.com>
2021-06-04 10:52:03 +0700

    Update

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-ini_set('display_errors', 1); 
-error_reporting(E_ALL);
 require_once "./src/core/App.php";
 require_once "./src/core/Controller.php";
 require_once "./src/core/Config.php";
